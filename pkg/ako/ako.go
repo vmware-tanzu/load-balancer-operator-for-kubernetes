958e8a600d43853c1d696fdf88dfe30dd76abb4b
Xudong Liu <xudongl@vmware.com>
2021-11-14 20:04:16 -0800

    adapt ako's fix for avi resource deletion in k8s 1.22
    Signed-off-by: Xudong Liu <xudongl@vmware.com>

--- pkg/ako/ako.go
+++ pkg/ako/ako.go
@@ -11,14 +11,15 @@ import (
 	"github.com/go-logr/logr"
 	akoov1alpha1 "github.com/vmware-samples/load-balancer-operator-for-kubernetes/api/v1alpha1"
 	appv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
-	akoStatefulSetName = "ako"
-	akoConditionType   = appv1.StatefulSetConditionType("ako.vmware.com/ObjectDeletionInProgress")
+	akoStatefulSetName         = "ako"
+	akoCleanUpAnnotationKey    = "AviObjectDeletionStatus"
+	akoCleanUpInProgressStatus = "Started"
+	akoCleanUpFinishedStatus   = "Done"
 )
 
 func CleanupFinished(ctx context.Context, remoteClient client.Client, log logr.Logger) (bool, error) {
@@ -36,14 +37,5 @@ func CleanupFinished(ctx context.Context, remoteClient client.Client, log logr.L
 		return false, err
 	}
 
-	return conditionHasStatus(ss.Status.Conditions, akoConditionType, corev1.ConditionFalse), nil
-}
-
-func conditionHasStatus(conditions []appv1.StatefulSetCondition, ctype appv1.StatefulSetConditionType, status corev1.ConditionStatus) bool {
-	for _, c := range conditions {
-		if c.Type == ctype && c.Status == status {
-			return true
-		}
-	}
-	return false
+	return ss.Annotations[akoCleanUpAnnotationKey] == akoCleanUpFinishedStatus, nil
 }
