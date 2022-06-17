25fab80d473aba3cefc0d4815062421ef1059e9a
Xudong Liu <xudongl@vmware.com>
2022-06-16 18:05:25 -0700

    build test case framework
    Signed-off-by: Xudong Liu <xudongl@vmware.com>

--- api/v1alpha1/akodeploymentconfig_webhook_test.go
+++ api/v1alpha1/akodeploymentconfig_webhook_test.go
@@ -3,9 +3,61 @@
 
 package v1alpha1
 
-func test() {
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+)
+
+func beforeEach(t *testing.T) (staticADC AKODeploymentConfig, g *WithT) {
 	runTest = true
-	if runTest {
-		print("run test")
+	staticADC = AKODeploymentConfig{}
+	g = NewWithT(t)
+	return staticADC, g
+}
+
+func TestCreateNewAKODeploymentConfig(t *testing.T) {
+	_, g := beforeEach(t)
+
+	testcases := []struct {
+		name      string
+		adc       AKODeploymentConfig
+		expectErr bool
+		expectMsg string
+	}{}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.adc.ValidateCreate()
+			if !tc.expectErr {
+				g.Expect(err).To(BeNil())
+			} else {
+				g.Expect(err).Should(HaveOccurred())
+			}
+		})
+	}
+}
+
+func TestUpdateExistingAKODeploymentConfig(t *testing.T) {
+	_, g := beforeEach(t)
+
+	testcases := []struct {
+		name      string
+		old       AKODeploymentConfig
+		new       AKODeploymentConfig
+		expectErr bool
+		expectMsg string
+	}{}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.new.ValidateUpdate(&tc.old)
+
+			if !tc.expectErr {
+				g.Expect(err).To(BeNil())
+			} else {
+				g.Expect(err).Should(HaveOccurred())
+			}
+		})
 	}
 }
